2a5a99aa54d1d57aa18512cd43e1ff8b7872ca64
CoinKing-Dream <yosukekuroki000@gmail.com>
2024-11-25 21:56:50 +0900

    feat: add divide

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,23 @@
 import ZojaxLogo from "../components/ZojaxLogo";
 import DivideLine from "../components/DivideLine";
+import Cards from "../components/Cards";
+import Partners from "../components/Partners";
 
 const Home = () => {
   return (
     <div className="w-full">
       <ZojaxLogo />
-      <DivideLine text={""}/>
+      <DivideLine text={""} />
+      <div className="w-full">
+        <div className="w-[50%] flex flex-col justify-center items-center">
+          <div>Copy Trading</div>
+          <div>Join the World's Top Crypto Trading Experts</div>
+        </div>
+        <div>
+
+        </div>
+      </div>
+      <Partners />
     </div>
   );
 };
