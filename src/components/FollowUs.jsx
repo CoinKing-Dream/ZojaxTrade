cab28a51b13f176d92a63f42fd668d28395c2d1e
CoinKing-Dream <yosukekuroki000@gmail.com>
2024-11-30 14:52:06 +0900

    style: update style of FollwUs

--- src/components/FollowUs.jsx
+++ src/components/FollowUs.jsx
@@ -4,21 +4,20 @@ const FollowUs = () => {
     // const ImgList = [YouTubeImg, TwitterImg, FaceBookImg]
     const ImgList = [FaceBookImg]
     return (
-        <div className="flex justify-end items-center md:w-[185px] sm:w-[140px] w-[100px] md:h-[60px] sm:h-[50px] h-[40px] relative text-white ">
-            <div className="absolute inset-0 bg-gradient-to-r from-gray-100 to-transparent rounded-[30px]" />
-            <div className="relative rounded-[30px] md:w-[170px] w-[130px] md:h-[50px] sm:h-[40px] h-[35px]">
-                <a className="cursor-pointer absolute left-[-10px]" href="#">
+        <div className="flex justify-center md:w-[160px] sm:w-[140px] w-[120px] md:h-[60px] sm:h-[50px] h-[37px] relative text-white bg-gradient-to-r from-gray-100 to-transparent p-1 rounded-[30px]">
+            <div className="relative flex items-center rounded-[30px] w-full h-full">
+                <a className="cursor-pointer absolute left-[0px]" href="#">
                     {
                         ImgList.map((item, idx) => (
                             <img
                                 src={item}
-                                className="md:w-[50px] sm:w-[40px] w-[35px]"
+                                className="md:w-[50px] sm:w-[40px] w-[30px]"
                                 key={idx}
                             />
                         ))
                     }
                 </a>
-                <div className="md:w-[150px] w-[120px] md:h-[50px] sm:h-[40px] h-[35px] bg-[#357ca8] rounded-[25px] flex justify-end items-center pr-3 md:text-[20px] sm:text-[18px] text-[15px]">
+                <div className="w-full h-full bg-[#357ca8] rounded-[25px] flex justify-end items-center pr-3 md:text-[20px] sm:text-[18px] text-[15px]">
                     Follow Us
                 </div>
             </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div className="w-full flex flex-col items-center">
       <Header />
-      <div className="w-full flex xl:flex-row xl:items-center xl:justify-around flex-col relative md:mt-[80px] sm:mt-[60px] mt-[40px] gap-5">
+      <div className="w-full flex xl:flex-row xl:items-center xl:justify-around flex-col relative xl:mt-[80px] sm:mt-[60px] mt-[40px] gap-5">
         <CopyTradingText />
         <Cards />
       </div>
