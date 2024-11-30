d19cfe86d9e996ae3a0e379c7c7a7f157e4b00d0
CoinKing-Dream <yosukekuroki000@gmail.com>
2024-11-30 20:46:38 +0900

    feat: update cards

--- src/sections.jsx/CopyTradingText.jsx
+++ src/sections.jsx/CopyTradingText.jsx
@@ -3,10 +3,10 @@ import GuidePDF from "../assets/Presentation.mp4";
 const CopyTradingText = () => {
   return (
     <div className="2xl:w-[50%] w-[100%] flex flex-col z-[11] items-center">
-      <p className="2xl:ml-[100px] xl:ml-[50px] px-10 xl:text-[40px] md:text-[40px] text-[30px] text-center xl:text-left xl:mt-10 text-white font-bold drop-shadow-[0_5px_2px_rgba(0,0,0,0.75)]">
+      <p className="2xl:ml-[100px] xl:ml-[50px] xl:text-[30px] md:text-[30px] sm:text-[25px] text-[15px] text-center xl:mt-10 text-white font-bold drop-shadow-[0_5px_2px_rgba(0,0,0,0.75)]">
         Dogs Lovers Cryptocurrency Investment Fund
       </p>
-      <p className="2xl:ml-[100px] xl:ml-[50px] indent-1 xl:w-[100%] lg:w-[70%] sm:w-[80%] w-[90%] xl:text-[20px] md:text-[18px] sm:text-[15px] text-[13px]  text-left xl:text-left mt-10 px-4 text-[white]">
+      <p className="flex flex-col md:gap-4 sm:gap-3 gap-2 2xl:ml-[100px] xl:ml-[50px] xl:w-[100%] lg:w-[70%] w-[90%] xl:text-[20px] md:text-[18px] sm:text-[15px] text-[13px]  text-left xl:text-left mt-5 px-4 text-[white]">
         <div>
           Invest in dog meme coins such as Doge and Shiba and use a custom
           Trading Bot to automate your trades, 24X7.
