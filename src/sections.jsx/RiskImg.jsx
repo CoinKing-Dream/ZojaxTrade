2415625723f2e80ee1a4a94f413b80b532c61b5b
CoinKing-Dream <yosukekuroki000@gmail.com>
2024-12-03 14:14:01 +0900

    feat: add high risk image

--- src/sections.jsx/RiskImg.jsx
+++ src/sections.jsx/RiskImg.jsx
@@ -1,4 +1,5 @@
 import lowImg from "../assets/risks/low.png";
+import highImg from "../assets/risks/high.png";
 
 const RiskImg = () => {
   return (
@@ -6,7 +7,7 @@ const RiskImg = () => {
       <div className="flex flex-row justify-center items-center 2xl:w-[50%] w-full gap-1">
         <img src={lowImg} className="cursor-pointer 2xl:w-[170px] xl:w-[140px] lg:w-[170px] md:w-[150px] w-[23%] rounded-lg border-1 drop-shadow-[20px_20px_10px_rgba(50,50,50,0.85)] hover:scale-[1.02]"/>
         <img src={lowImg} className="cursor-pointer 2xl:w-[170px] xl:w-[140px] lg:w-[170px] md:w-[150px] w-[23%] rounded-lg border-1 drop-shadow-[20px_20px_10px_rgba(50,50,50,0.85)] hover:scale-[1.02]" />
-        <img src={lowImg} className="cursor-pointer 2xl:w-[170px] xl:w-[140px] lg:w-[170px] md:w-[150px] w-[23%] rounded-lg border-1 drop-shadow-[20px_20px_10px_rgba(50,50,50,0.85)] hover:scale-[1.02]" />
+        <img src={highImg} className="cursor-pointer 2xl:w-[170px] xl:w-[140px] lg:w-[170px] md:w-[150px] w-[23%] rounded-lg border-1 drop-shadow-[20px_20px_10px_rgba(50,50,50,0.85)] hover:scale-[1.02]" />
       </div>
     </>
   );
