ed1211950e3a7694bb4deeb1beeab5e4f7fbe7b6
CoinKing-Dream <yosukekuroki000@gmail.com>
2024-12-04 01:49:16 +0900

    feat: update

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,56 +1,102 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectCards, Navigation } from "swiper/modules";
-import "swiper/swiper-bundle.css";
-import 'swiper/css/navigation';
-import "swiper/css/effect-cards";
+import {
+  Navigation,
+  Pagination,
+  Autoplay,
+  EffectCube,
+  Scrollbar,
+  A11y,
+  Keyboard,
+} from "swiper/modules";
+
 import firstCard from "../assets/cards/1.png";
 import secondCard from "../assets/cards/2.png";
 import thirdCard from "../assets/cards/3.png";
 import fourthCard from "../assets/cards/4.png";
 import fifthCard from "../assets/cards/5.png";
-import leftRedArrowImg from "../assets/left-red-arrow.png";
-import rightRedArrowImg from "../assets/right-red-arrow.png";
-import leftBrownArrowImg from "../assets/left-brown-arrow.png";
-import rightBrownArrowImg from "../assets/right-brown-arrow.png";
+
+import "swiper/css";
+import "swiper/swiper-bundle.css";
+import "swiper/css/effect-cards";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 const Cards = () => {
-  const [currentSlide, setCurrentSlide] = useState(0)
+  const progressCircle = useRef(null);
+  const fractionRef = useRef(null);
+  const onAutoplayTimeLeft = (s, time, progress) => {
+    progressCircle.current.style.setProperty("--progress", 1 - progress);
+  };
+
   return (
     <div className="relative flex xl:justify-end justify-center z-100">
       <div className="2xl:w-[500px] xl:w-[400px] lg:w-[500px] md:w-[450px] w-[60vw]">
         <Swiper
-          effect={"cards"}
-          grabCursor={true}
-          modules={[EffectCards, Navigation]}
-          navigation={{
-            nextEl: ".swiper-button-next1",
-            prevEl: ".swiper-button-prev1",
+          // loop={true}
+          effect="cube"
+          keyboard={{
+            enabled: true,
           }}
-          className="flex flex-row items-center"
-          onSlideChange={(swiper) => setCurrentSlide(swiper?.activeIndex)}
+          pagination={{
+            clickable: true,
+            type: "bullets",
+          }}
+          navigation={true}
+          modules={[EffectCube, Autoplay, Keyboard, Pagination, Navigation]}
+          className="mySwiper"
+          onAutoplayTimeLeft={onAutoplayTimeLeft}
+          centeredSlides={true}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+          onPaginationUpdate={(swiper) => {
+            fractionRef.current.textContent = `${swiper.activeIndex + 1} / 5`;
+          }}
         >
           <SwiperSlide className="rounded-[10px] sm:rounded-[20px] border-[1px] border-blue-400 drop-shadow-[2px_3px_5px_rgba(200,200,200,0.85)]">
-            <img src={firstCard} alt="First card" />
+            <img
+              src={firstCard}
+              alt="First card"
+              className="rounded-[5px] sm:rounded-[10px]"
+            />
           </SwiperSlide>
           <SwiperSlide className="rounded-[10px] sm:rounded-[20px] border-[1px] border-blue-400 drop-shadow-[2px_3px_5px_rgba(200,200,200,0.85)]">
-            <img src={secondCard} alt="Second card" />
+            <img
+              src={secondCard}
+              alt="Second card"
+              className="rounded-[5px] sm:rounded-[10px]"
+            />
           </SwiperSlide>
           <SwiperSlide className="rounded-[10px] sm:rounded-[20px] border-[1px] border-blue-400 drop-shadow-[2px_3px_5px_rgba(200,200,200,0.85)]">
-            <img src={thirdCard} alt="Third card" />
+            <img
+              src={thirdCard}
+              alt="Third card"
+              className="rounded-[5px] sm:rounded-[10px]"
+            />
           </SwiperSlide>
           <SwiperSlide className="rounded-[10px] sm:rounded-[20px] border-[1px] border-blue-400 drop-shadow-[2px_3px_5px_rgba(200,200,200,0.85)]">
-            <img src={fourthCard} alt="Fourth card" />
+            <img
+              src={fourthCard}
+              alt="Fourth card"
+              className="rounded-[5px] sm:rounded-[10px]"
+            />
           </SwiperSlide>
           <SwiperSlide className="rounded-[10px] sm:rounded-[20px] border-[1px] border-blue-400 drop-shadow-[2px_3px_5px_rgba(200,200,200,0.85)]">
-            <img src={fifthCard} alt="Fifth card" />
+            <img
+              src={fifthCard}
+              alt="Fifth card"
+              className="rounded-[5px] sm:rounded-[10px]"
+            />
           </SwiperSlide>
-          <div className={`${currentSlide==4?"opacity-80":"opacity-100"} cursor-pointer flex justify-center items-center swiper-button-next1 absolute top-[50%] right-[-100px] sm:right-[-120px] cursor-pointer mx-12 z-10000`}>
-            <img src={currentSlide==4?rightBrownArrowImg:rightRedArrowImg} className="sm:w-[45px] sm:h-[15px] w-[35px] h-[10px]"/>
+          <div className="autoplay-progress" slot="container-end">
+            <svg viewBox="0 0 26 26" ref={progressCircle}>
+              <circle cx="10" cy="10" r="8"></circle>
+            </svg>
           </div>
-          <div className={`${currentSlide==0?"opacity-80":"opacity-100"} cursor-pointer flex justify-center items-center swiper-button-prev1 absolute top-[50%] left-[-100px] sm:left-[-120px] cursor-pointer mx-12 z-10000`}>
-            <img src={currentSlide==0?leftBrownArrowImg:leftRedArrowImg} className="sm:w-[45px] sm:h-[15px] w-[35px] h-[10px]"/>
-          </div>
+          <div className="custom-fraction" ref={fractionRef}></div>
         </Swiper>
       </div>
     </div>
